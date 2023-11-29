60a9fa54b060ed7d8b884bf15144b9e73ab848c5
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-11-29 14:19:40 +0000

    design: display skills in marquee

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,18 +1,35 @@
 <template>
-    <div class="container py-20">
-        <h2 class="text-4xl lg:text-5xl xl:text-6xl text-white">Not your regular developer! <span class="text-primary">Skill Sets</span></h2>
-
-        <div class="flex space-x-4 items-end">
-            <div class="flex flex-col items-center space-y-4 grayscale transition duration-200 hover:grayscale-0 cursor-pointer" v-for="(tool, index) in data.tools" :key="index">
-                <div class="w-40">
-                    <img :src="`/images/tools-and-technologies/${tool.image}`" :alt="tool.title" class="w-full object-cover object-center">
-                </div>
-                <span class="text-white text-lg font-light">{{ tool.title }}</span>
-            </div>
+    <div class="py-20 space-y-4">
+        <div class="container">
+            <h2 class="text-4xl lg:text-5xl xl:text-6xl text-white">
+                Not your regular developer!
+                <span class="text-primary">Skill Sets</span>
+            </h2>
         </div>
+
+        <Vue3Marquee duration="50">
+            <div class="flex space-x-10 items-end">
+                <div
+                    class="flex flex-col items-center space-y-4 grayscale transition duration-200 hover:grayscale-0 cursor-pointer"
+                    v-for="(tool, index) in data.tools"
+                    :key="index"
+                >
+                    <div class="w-40">
+                        <img
+                            :src="`/images/tools-and-technologies/${tool.image}`"
+                            :alt="tool.title"
+                            class="w-full object-cover object-center"
+                        />
+                    </div>
+                    <span class="text-white text-lg font-light">{{
+                        tool.title
+                    }}</span>
+                </div>
+            </div>
+        </Vue3Marquee>
     </div>
 </template>
 
 <script setup lang="ts">
-const { data } = await useFetch('/api/tools')
+const { data } = await useFetch("/api/tools");
 </script>
