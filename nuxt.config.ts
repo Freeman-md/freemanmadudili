d676dca7a87f0dc105587237fb466e84f20cb482
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-11-30 00:25:08 +0000

    config: integrate google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@ import svgLoader from 'vite-svg-loader'
 
 export default defineNuxtConfig({
   ssr: true,
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-49MQ1SP9TJ"
+        },
+        {
+          src: '/js/google-analytics.js'
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
   vite: {
     plugins: [
