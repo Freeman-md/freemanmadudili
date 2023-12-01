4a31fe6077099901187983ed7edc30fd0ac953da
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-12-01 18:10:45 +0000

    design: update quick contact component

--- components/QuickContact.vue
+++ components/QuickContact.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="mailto:hello@freemanmadudili.com?subject=Exciting Opportunity Available" class="fixed bottom-4 right-4 z-10 w-16 h-16 rounded-full border-2 border-primary text-primary flex items-center justify-center transition duration-200 hover:bg-primary/10">
+    <a href="mailto:hello@freemanmadudili.com?subject=Exciting Opportunity Available" class="fixed bottom-4 right-4 z-10 w-16 h-16 rounded-full border-2 border-primary text-primary flex items-center justify-center transition duration-200 hover:bg-primary/10 backdrop-blur-md">
         <MailIcon class="w-10" />
     </a>
 </template>
