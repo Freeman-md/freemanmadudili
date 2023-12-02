3079ba529a7dc843998570ef1a59e403516972f1
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-12-02 13:58:23 +0000

    design: update featured project

--- components/FeaturedProject.vue
+++ components/FeaturedProject.vue
@@ -2,15 +2,16 @@
     <div class="group grayscale xs:w-48 sm:w-64 md:w-80 lg:w-60 xl:w-72 transition duration-200 hover:!grayscale-0">
         <div class="relative rounded-lg border-4 border-smoky xs:w-48 xs:h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-60 lg:h-60 xl:w-72 xl:h-72 transition duration-200 group-hover:!border-transparent">
             
-            <div
-                class="transform translate-y-4 translate-x-4 xs:w-48 xs:h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-60 lg:h-60 xl:w-72 xl:h-72 overflow-hidden rounded-lg transition duration-200 group-hover:!translate-x-0 group-hover:!translate-y-0"
+            <a
+                :href="project?.links.live || project?.links.github"
+                class="block transform translate-y-4 translate-x-4 xs:w-48 xs:h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-60 lg:h-60 xl:w-72 xl:h-72 overflow-hidden rounded-lg transition duration-200 group-hover:!translate-x-0 group-hover:!translate-y-0"
             >
                 <img
                     :src="project?.image"
                     :alt="project?.title"
                     class="object-cover w-full h-full"
                 />
-            </div>
+            </a>
         </div>
 
         <div class="space-y-4 pt-10 sm:pt-16 w-full overflow-hidden">
@@ -18,12 +19,12 @@
                 <span class="text-xl text-smoky">{{ project?.title }}</span>
 
                 <div class="flex space-x-2 items-center stroke-smoky">
-                    <a href="">
+                    <a :href="project?.links.github">
                         <GithubIcon
                             class="w-8 stroke-smoky transition duration-200 hover:stroke-primary"
                         />
                     </a>
-                    <a href="">
+                    <a :href="project?.links.live">
                         <ExternalLinkIcon
                             class="stroke-smoky transition duration-200 hover:stroke-primary"
                         />
