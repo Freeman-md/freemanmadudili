b664e492804182b9677758e8f9caa96ff86c4f56
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-02-16 02:04:44 +0000

    update hero section

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,36 +1,41 @@
 <template>
-    <section id="hero" class="container py-20 lg:py-32 grid md:grid-cols-2 lg:grid-cols-3 gap-6 items-center">
+    <section id="hero" class="container py-20 lg:py-32 space-y-8">
 
-        <div class="lg:col-span-2 flex flex-col space-y-8">
+        <p class="text-primary text-lg text-center md:text-left">— Freeman Madudili —</p>
 
-            <p class="text-primary text-lg">— Freeman Madudili</p>
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6 items-center">
 
-            <h1 class="text-5xl md:text-7xl text-smoky">
-                I'm a <span class="text-primary">
-                    Certified Full Stack Developer
-                </span>
-            </h1>
+            <div class="lg:col-span-2 flex flex-col space-y-8 items-start">
 
-            <p class="text-white">
-                A distinguished Full Stack Developer with over <strong>3 years of experience</strong> and a <strong>First
-                    Class Honours</strong> degree in <strong>Information Technology</strong> from Middlesex University.
-                Expert in crafting advanced web solutions, evidenced by numerous successful projects. Awarded a
-                <strong>Distinction</strong> in <strong>BTEC Level 3 Advanced Diploma in Software Engineering</strong> from
-                Aptech Computer Education.
-            </p>
+                <h1 class="text-5xl md:text-7xl text-smoky">
+                    I'm a <span class="text-primary">
+                        Certified Full Stack Developer
+                    </span>
+                </h1>
 
-            <SocialLinks class="inline-block" />
+                <p class="text-white">
+                    A distinguished Full Stack Developer with over <strong>3 years of experience</strong> and a
+                    <strong>First
+                        Class Honours</strong> degree in <strong>Information Technology</strong> from Middlesex University.
+                    Expert in crafting advanced web solutions, evidenced by numerous successful projects. Awarded a
+                    <strong>Distinction</strong> in <strong>BTEC Level 3 Advanced Diploma in Software Engineering</strong>
+                    from
+                    Aptech Computer Education.
+                </p>
 
-            <NuxtLink to="#skills" class="text-white underline text-sm">More about me</NuxtLink>
+                <SocialLinks class="inline-block" />
 
-        </div>
+                <a href="/freeman-madudili-cv.pdf" target="_blank" class="btn btn-primary">My Resume</a>
 
-        <div class="order-first md:order-last flex flex-col space-y-4 items-center justify-center">
-            <div class="w-1/2 md:w-full md:h-96 overflow-hidden rounded-xl">
-                <img src="/images/banner.jpg" alt="Freeman Madudili" class="object-contain object-center w-full" />
             </div>
-            <p class="text-smoky">A.K.A <span class="text-primary text-lg">Freemancodz</span></p>
-            <a href="/freeman-madudili-cv.pdf" target="_blank" class="btn btn-primary">My Resume</a>
+
+            <div class="order-first md:order-last flex flex-col space-y-4 items-center justify-center">
+                <div class="w-1/2 md:w-full md:h-96 overflow-hidden rounded-xl">
+                    <img src="/images/banner.jpg" alt="Freeman Madudili" class="object-contain object-center w-full" />
+                </div>
+                <p class="text-smoky">A.K.A <span class="text-primary text-lg">Freemancodz</span></p>
+            </div>
+
         </div>
 
     </section>
